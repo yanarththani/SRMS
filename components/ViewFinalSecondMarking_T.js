8d65d2441520e24c131aa8dc3ccec11dd5808436
Kasthoo2000 <87089525+Kasthoo2000@users.noreply.github.com>
2024-03-12 10:11:36 +0530

    viewFinalmarking_T

---
+++ components/ViewFinalSecondMarking_T.js
@@ -0,0 +1,31 @@
+export default function ViewFinalSecondMarking_T(){
+
+    return(
+        <div>
+
+            <h1>Final Second Marking</h1>
+
+            <table border={1}>
+            <thead>
+                <tr>
+                    <td>S.No</td>
+                    <td>Student indexNo</td>
+                    <td>Q1</td>
+                    <td>Q2</td>
+                    <td>Q3</td>
+                    <td>Q4</td>
+                    <td>Q5</td>
+                    <td>Q6</td>
+                    <td>Toatal</td>
+                    <td>Average</td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    
+                </tr>
+            </tbody>
+        </table>
+        </div>
+    )
+}
