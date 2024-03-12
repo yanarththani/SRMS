007a574f5ac77cfc7a0aae51ac2bdfb5085a8c58
D-Hiruni <epramodya123@gmail.com>
2024-03-12 12:54:20 +0530

    Final_marks_P

---
+++ components/FinalFirstMarking_P.js
@@ -0,0 +1,27 @@
+export default function FinalFirstMarking_P(){
+    return(
+            <div className=''>
+                <h2>Final Marks</h2>
+                <div className=''>
+                    <form action="#">
+                        <div className="">
+                            <label>S.No</label>
+                            <input type="text" name="s_no"/>
+                        </div>
+                        <div className="">
+                            <label>Student indexNo</label>
+                            <input type="text" name="stu_indexNo"/>
+                        </div>
+                        <div className="">
+                            <label>Total</label>
+                            <input type="text" name="total_marks"/>
+                        </div>
+                        <div className="">
+                            <label>Average</label>
+                            <input type="text" name="average"/>
+                        </div>
+                    </form>
+                </div>
+            </div>
+    )
+}
