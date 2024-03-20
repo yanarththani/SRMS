ca1e7721ab1fd687015913706687e85440dfc6d7
Jerlin-Grashiya <149485847+Jerlin-Grashiya@users.noreply.github.com>
2024-03-20 19:19:44 +0530

    Add files via upload

---
+++ components/ViewICAEMarks_T.js
@@ -0,0 +1,27 @@
+export default function ViewICAEMarks_T(){
+
+    return(
+        
+        <div>
+           
+                <h1>ICAE Marks</h1>
+            
+            <table border={1}>
+            <thead>
+                <tr>
+                    <td>S.No</td>
+                    <td>Student IndexNo</td>
+                    <td>ICAE 01</td>
+                    <td>ICAE 02</td>
+                    <td>ICAE 03</td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    
+                </tr>
+            </tbody>
+        </table>
+        </div>
+    )
+}
