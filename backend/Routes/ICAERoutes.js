f1baa4de489e2b8f3f5dd314f5a1247ac386ca63
Jerlin-Grashiya <jerlingrashiya@gmail.com>
2024-05-29 15:45:29 +0530

    Modified ICAERouter.js

--- backend/Routes/ICAERoutes.js
+++ backend/Routes/ICAERoutes.js
@@ -1,13 +1,11 @@
-// icamarksRouter.js
 const express = require('express');
 const router = express.Router();
 const icamarksController = require('../Controller/ICAEController');
 
 // Routes
 router.post('/add', icamarksController.addICAMarks);
-router.get('/', icamarksController.getICAMarks);
-router.get('/:id', icamarksController.getICAMarksById);
+router.get('/:course', icamarksController.getICAMarksByCourse);
 router.put('/:id', icamarksController.updateICAMarks);
-router.delete('/:id', icamarksController.deleteICAMarks);
+router.delete('/:regNo', icamarksController.deleteICAMarks);
 
 module.exports = router;
