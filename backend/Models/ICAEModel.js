75df6240925461a3834e444a50730fb4dad25791
Jerlin-Grashiya <jerlingrashiya@gmail.com>
2024-05-29 15:40:32 +0530

    Modify ICAEModel.js

--- backend/Models/ICAEModel.js
+++ backend/Models/ICAEModel.js
@@ -1,13 +1,15 @@
-// ICAMarks.js
 const mongoose = require('mongoose');
 
 const icamarksSchema = new mongoose.Schema({
-  regNo: { type: String,  },
-  ica01: { type: Number, },
-  ica02: { type: Number, },
-  ica03: { type: Number,  },
-  totalMarks: { type: Number,  },
-  averageMarks: { type: Number, }
+  course: { type: String },
+  marks: [{
+    regNo: { type: String },
+    ica01: { type: Number },
+    ica02: { type: Number },
+    ica03: { type: Number },
+    totalMarks: { type: Number },
+    averageMarks: { type: Number }
+  }]
 });
 
 const ICAMarks = mongoose.model('ICAMarks', icamarksSchema);
